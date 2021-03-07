59ebf1c7dc05125d90577ad23b7a0fbdb9bea680
Cheng Chen <chengchenww@gmail.com>
2021-03-08 03:24:45 +1000

    Basic apt update and install.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Setup
+
+Setup the basics for a new device.
+
+```
+sh essentials.sh
+./essentials.sh
+```
---
+++ essentials.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/sh
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+
+sudo apt update
+sudo apt install vim git zsh -y
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# sed
+
+curl -sLf https://spacevim.org/install.sh | bash
+cd ~/.SpaceVim/bundle/vimproc.vim
+make
